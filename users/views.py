829bc1e7ca1a7e385fa2b795a5167a4a52798d22
clencyc <clency2023@gmail.com>
2024-12-06 21:54:20 +0300

    added sign up

--- users/views.py
+++ users/views.py
@@ -1,26 +1,35 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login, authenticate
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
 
 def signup(request):
     if request.method == 'POST':
-        name = request.POST['name']
+        username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
-        user = User.objects.create_user(email=email, password=password)
-        login(request, user)
-        return redirect('home')  # Replace 'home' with your home view name
+        
+        if password != confirm_password:
+            return render(request, 'signup.html', {'error': 'Passwords do not match'})
+        
+        username = email.split('@')[0]  # Use part of the email as username
+        user = User.objects.create_user(username=username, email=email, password=password)
+        auth_login(request, user)
+        return redirect('home')
     return render(request, 'signup.html')
 
-def login(request):
+def user_login(request):
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
         user = authenticate(request, email=email, password=password)
         if user is not None:
-                login(request, user)
+                auth_login(request, user)
                 return redirect('home')
         else:
             return render(request, 'login.html', {'error': 'Invalid credentials'})
     return render(request, 'login.html')
+
+
+def home(request):
+    return render(request, 'home.html')
