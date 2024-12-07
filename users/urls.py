9b6a6afb596c16ea7f2feb903c09340098e2d351
clencyc <clency2023@gmail.com>
2024-12-07 10:03:46 +0300

    added sign up

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
+    path('login/', views.user_login, name='user_login'),
     path('home/', views.home, name='home'),
     # path('logout/', views.logout, name='logout'),
 
